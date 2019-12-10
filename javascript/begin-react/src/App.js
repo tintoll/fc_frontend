1ebe06e4b56d9541dfa25e6073ef3b61eecbd155
tintoll <belselios@gmail.com>
2019-12-10 15:33:49 +0900

    Immer 이용하기

--- javascript/begin-react/src/App.js
+++ javascript/begin-react/src/App.js
@@ -2,7 +2,7 @@ import React, { useReducer, useMemo } from 'react';
 import './App.css';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
-
+import produce from 'immer';
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
@@ -48,22 +48,39 @@ function reducer(state, action) {
         }
       };
     case 'CREATE_USER' : 
+      /*
       return {
         inputs : initialState.inputs,
         users : state.users.concat(action.user)
       };
+      */
+      return produce(state, draft => {
+        draft.user.push(action.user);
+      });
     case 'TOGGLE_USER':
+      /*
       return {
         ...state,
         users: state.users.map(user =>
           user.id === action.id ? { ...user, active: !user.active } : user
         )
       };
+      */
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;  
+      });
     case 'REMOVE_USER':
+      /*
       return {
         ...state,
         users: state.users.filter(user => user.id !== action.id)
-      };      
+      };
+      */
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id !== action.id);
+        draft.users.splice(index, 1);
+      });    
     default :
       return state;
   }
