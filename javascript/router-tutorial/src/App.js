9d2974eb03d55a1a9f50ca27f43a1d363f65ac77
tintoll <belselios@gmail.com>
2019-12-23 08:55:23 +0900

    라우터 기능 실습

--- javascript/router-tutorial/src/App.js
+++ javascript/router-tutorial/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Switch,Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Profiles from './components/Profiles';
+import HistorySample from './components/HistorySample';
 
 function App() {
   return (
@@ -17,10 +18,24 @@ function App() {
         <li>
           <Link to="/profiles">프로필 목록</Link>
         </li>
+        <li>
+          <Link to="/history">히스토리 샘플</Link>
+        </li>
       </ul>
-      <Route path="/" component={Home}  exact />
-      <Route path="/about" component={About} />
-      <Route path="/profiles" component={Profiles} />
+      <Switch>
+        <Route path="/" component={Home}  exact />
+        <Route path="/about" component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route 
+          render={({location}) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다.</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )} 
+        />
+      </Switch>
     </div>    
   );
 }
