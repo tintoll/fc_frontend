05ad5b2130effcd60b34dd605d45d77f11272834
tintoll <belselios@gmail.com>
2019-12-31 10:29:56 +0900

    리덕스 개발자 도구 셋팅

--- javascript/learn-redux/src/index.js
+++ javascript/learn-redux/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import rootReducer from './module/index';
 import { Provider} from 'react-redux';
-
-const store = createStore(rootReducer); // 스토어를 만듭니다.
-
+import { composeWithDevTools } from 'redux-devtools-extension'
+const store = createStore(rootReducer, composeWithDevTools() ); // 스토어를 만듭니다.
+// composeWithDevTools 를 사용하여 리덕스 개발자 도구 활성화
 ReactDOM.render(
     <Provider store={store}>
         <App />
