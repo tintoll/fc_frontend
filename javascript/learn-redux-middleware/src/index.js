d2a0fbcf23a7909286f5b5c4d920f724f3b0229b
tintoll <belselios@gmail.com>
2020-01-03 09:47:14 +0900

    redux-logger 적용및 devtools 적용

--- javascript/learn-redux-middleware/src/index.js
+++ javascript/learn-redux-middleware/src/index.js
@@ -6,9 +6,15 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './modules/index';
-import myLogger from './middlewares/myLogger';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-const store = createStore(rootReducer, applyMiddleware(myLogger));
+const store = createStore(rootReducer, 
+    composeWithDevTools(
+        applyMiddleware(logger),
+        // other store enhancers if any
+    )    
+);
 
 ReactDOM.render(
     <Provider store={store}>
