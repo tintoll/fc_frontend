386a803d735cb9c6f42a61c5a0c2bdb1bd14b97b
tintoll <belselios@gmail.com>
2020-01-06 08:40:34 +0900

    redux-thunk을 이용한 Counter 비동기작업

--- javascript/learn-redux-middleware/src/index.js
+++ javascript/learn-redux-middleware/src/index.js
@@ -8,10 +8,11 @@ import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './modules/index';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension'
+import ReduxThunk  from 'redux-thunk';
 
 const store = createStore(rootReducer, 
     composeWithDevTools(
-        applyMiddleware(logger),
+        applyMiddleware(ReduxThunk,logger),
         // other store enhancers if any
     )    
 );
