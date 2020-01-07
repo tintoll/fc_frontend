6da4b07bac108050e9997dca7220d8031242a90f
tintoll <belselios@gmail.com>
2020-01-07 09:36:14 +0900

    api 재로딩 문제해결하기

--- javascript/learn-redux-middleware/src/modules/posts.js
+++ javascript/learn-redux-middleware/src/modules/posts.js
@@ -1,9 +1,8 @@
 import * as postsAPI from '../api/posts'; // api/posts 안의 함수 모두 불러오기
-import { createPromiseThunk, reducerUtils } from '../lib/asyncUtils'
+import { createPromiseThunk, reducerUtils, createPromiseThunkById, handleAsyncActionsById } from '../lib/asyncUtils'
 import { handleAsyncActions } from './../lib/asyncUtils';
 
 /* 액션 타입 */
-
 // 포스트 여러개 조회하기
 const GET_POSTS = 'GET_POSTS'; // 요청 시작
 const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS'; // 요청 성공
@@ -16,7 +15,7 @@ const GET_POST_ERROR = 'GET_POST_ERROR';
 
 // 아주 쉽게 thunk 함수를 만들 수 있게 되었습니다.
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
 
 const initialState = {
   posts: reducerUtils.initial(),
@@ -28,11 +27,11 @@ export default function posts(state = initialState, action) {
       case GET_POSTS:
       case GET_POSTS_SUCCESS:
       case GET_POSTS_ERROR:
-        return handleAsyncActions(GET_POSTS, 'posts')(state, action);
+        return handleAsyncActions(GET_POSTS, 'posts', true)(state, action);
       case GET_POST:
       case GET_POST_SUCCESS:
       case GET_POST_ERROR:
-        return handleAsyncActions(GET_POST, 'post')(state, action);
+        return handleAsyncActionsById(GET_POST, 'post', true)(state, action);
       default:
         return state;
     }
