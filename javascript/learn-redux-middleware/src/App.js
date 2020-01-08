be73beaeebdf1cfe4aaba7a27f0fb3557240bd9f
tintoll <belselios@gmail.com>
2020-01-09 08:35:14 +0900

    redux-saga를 이용하여 카운터 만들기

--- javascript/learn-redux-middleware/src/App.js
+++ javascript/learn-redux-middleware/src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import PostListPage from './pages/PostListPage';
 import PostPage from './pages/PostPage';
+import CounterContainer from './components/CounterContainer';
 
 function App() {
   return (
     <>
-      <Route path="/" component={PostListPage} exact />
-      <Route path="/:id" component={PostPage} />
+      <CounterContainer />
+
+      
     </>
   );
 }
 
+/*
+<Route path="/" component={PostListPage} exact />
+      <Route path="/:id" component={PostPage} />
+*/
 export default App;
--- javascript/learn-redux-middleware/src/index.js
+++ javascript/learn-redux-middleware/src/index.js
@@ -5,24 +5,32 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import rootReducer from './modules/index';
+import rootReducer, { rootSaga } from './modules/index';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension'
 import ReduxThunk  from 'redux-thunk';
 import { Router } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
+import createSagaMiddleware from 'redux-saga';
 
 import axios from 'axios';
 axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? '/' : 'https://api.velog.io/';
 
 const customHistory = createBrowserHistory();
+const sagaMiddleware = createSagaMiddleware(); // 사가 미들웨어를 만듭니다.
 
 const store = createStore(rootReducer, 
     composeWithDevTools(
-        applyMiddleware(ReduxThunk.withExtraArgument({history:customHistory}),logger),
+        applyMiddleware(
+            ReduxThunk.withExtraArgument({history:customHistory})
+            , sagaMiddleware // 사가 미들웨어를 적용하고
+            , logger),
         // other store enhancers if any
     )    
 );
+
+// 주의: 스토어 생성이 된 다음에 위 코드를 실행해야합니다.
+sagaMiddleware.run(rootSaga); // 루트 사가를 실행해줍니다.
 
 ReactDOM.render(
     <Router history={customHistory} >
