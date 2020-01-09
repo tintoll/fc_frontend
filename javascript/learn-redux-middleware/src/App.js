fb791786f158d404229bfe64d89f6058fd4853a7
tintoll <belselios@gmail.com>
2020-01-09 09:45:03 +0900

    redux-saga로 프로미스 다루기

--- javascript/learn-redux-middleware/src/App.js
+++ javascript/learn-redux-middleware/src/App.js
@@ -2,20 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import PostListPage from './pages/PostListPage';
 import PostPage from './pages/PostPage';
-import CounterContainer from './components/CounterContainer';
 
 function App() {
   return (
     <>
-      <CounterContainer />
-
-      
+      <Route path="/" component={PostListPage} exact />
+      <Route path="/:id" component={PostPage} />
     </>
   );
 }
 
-/*
-<Route path="/" component={PostListPage} exact />
-      <Route path="/:id" component={PostPage} />
-*/
 export default App;
--- javascript/learn-redux-middleware/src/modules/posts.js
+++ javascript/learn-redux-middleware/src/modules/posts.js
@@ -1,7 +1,8 @@
 import * as postsAPI from '../api/posts'; // api/posts 안의 함수 모두 불러오기
-import { createPromiseThunk, reducerUtils, createPromiseThunkById, handleAsyncActionsById } from '../lib/asyncUtils'
-import { handleAsyncActions } from './../lib/asyncUtils';
-
+import { reducerUtils, handleAsyncActionsById } from '../lib/asyncUtils'
+import { handleAsyncActions,createPromiseSaga,createPromiseSagaById } from './../lib/asyncUtils';
+import { takeEvery } from 'redux-saga/effects'
+ 
 /* 액션 타입 */
 // 포스트 여러개 조회하기
 const GET_POSTS = 'GET_POSTS'; // 요청 시작
@@ -14,8 +15,25 @@ const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
 
 // 아주 쉽게 thunk 함수를 만들 수 있게 되었습니다.
+/*
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
 export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
+*/
+
+// redux-saga 이용
+export const getPosts =  () => ({type: GET_POSTS});
+// payload는 파라미터 용도, meta는 리듀서에서 id를 알기위한 용도
+export const getPost = id => ({type : GET_POST, payload:id, meta:id});
+
+const getPostsSaga = createPromiseSaga(GET_POSTS, postsAPI.getPosts);
+const getPostSaga = createPromiseSagaById(GET_POST, postsAPI.getPostById);
+
+export function* postsSaga() {
+  yield takeEvery(GET_POSTS, getPostsSaga);
+  yield takeEvery(GET_POST, getPostSaga);
+}
+
+
 export const goToHome = () => (dispatch, getState, {history}) => {
   history.push('/');
 }
